9ca12f9a91ae113168204b307beae144f170279e
Vssmllxs <samuelcuesta56782094@gmail.com>
2026-09-02 15:26:42 -0500

    Add LoginPage widget and refactor HomePage layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
+import 'layouts/login_page.dart';
 import 'layouts/presto_title.dart';
 
 void main() {
@@ -36,69 +37,13 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             Align(alignment: Alignment.topLeft, child: PrestoTitle()),
-
             SizedBox(
               height: 610,
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(24),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50),
-                  ),
-                ),
-
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
-
-                  child: Column(
-                    children: <Widget>[
-                      const SizedBox(height: 20),
-
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: const [],
-                        ),
-
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: SesionText,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              child: LoginPage(),
             ),
           ],
         ),
       ),
     );
-  }
-
-  List<Widget> get SesionText {
-    return <Widget>[
-      const Text(
-        "Inicio de sesion",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-
-      const Text(
-        "Bienvenido",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 14,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ];
   }
 }
